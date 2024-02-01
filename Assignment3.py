cc4866419321ac9cb91e2e0234921ec78da195b6
Libacl123 <libasheeja@gmail.com>
2024-02-01 10:11:01 +0530

    pushing assignment2

---
+++ Assignment3.py
@@ -0,0 +1,15 @@
+a=int(input("enter the no:"))
+b=int(input("enter the no:"))
+y=input("enter the operator:")
+if(y=="+"):
+    c=a+b
+    print(c)
+if(y=="-"):
+    c=a-b
+    print(c)
+if(y=="*"):
+    c=a*b
+    print(c)
+if(y=="/"):
+    c=a/b
+    print(c)
---
+++ Assignment5.py
@@ -0,0 +1,5 @@
+a=int(input("a:"))
+if(a%2==0):
+    print("Number is EVEN") 
+else:
+    print("Number is ODD")   
